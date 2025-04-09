cb8a22e8f115c6a4b506d7b79fe1d1740775f81d
Your Name <you@example.com>
2025-04-09 14:12:18 +0800

    update

--- workspace/ros2_example/src/example_pkg/setup.py
+++ workspace/ros2_example/src/example_pkg/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-package_name = 'my_first_py_pkg'
+package_name = 'example_pkg'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_namespace_packages(include=[package_name + '*']),
+    version='0.0.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,14 +13,16 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='panda',
-    maintainer_email='panda@todo.todo',
-    description='ROS2 turtle control package',
+    maintainer='root',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'turtle_controller = my_first_py_pkg.turtle_controller_node:main',
+            'publisher = example_pkg.publisher:main',
+            'subscriber = example_pkg.subscriber:main',
+            'service_node = example_pkg.service_node:main',
         ],
     },
 )
