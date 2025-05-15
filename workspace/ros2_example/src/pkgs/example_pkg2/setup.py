ee7e53bfd3abc03ab223bc85b94dbc443394ab3e
Your Name <you@example.com>
2025-05-15 21:42:28 +0800

    update

--- workspace/ros2_example/src/pkgs/example_pkg2/setup.py
+++ workspace/ros2_example/src/pkgs/example_pkg2/setup.py
@@ -51,10 +51,11 @@ setup(
     entry_points={
         'console_scripts': [
             # 格式: '命令名 = 模块路径:函数名'
-            'publisher = nodes.publisher_node.main:main',
-            'subscriber = nodes.subscriber_node.main:main',
-            'service_node = nodes.service_node.main:main',
-            'utils_example_node = nodes.utils_node.main:main',
+            'publisher = nodes.publisher:main',      # 发布者节点
+            'subscriber = nodes.subscriber:main',    # 订阅者节点
+            'service_node = nodes.service_node:main', # 服务节点
+            'utils_example = nodes.utils_example_node:main', # 工具函数示例节点
+            'light_control = nodes.light_control:main',  # 添加灯光控制节点
         ],
     },
 )
